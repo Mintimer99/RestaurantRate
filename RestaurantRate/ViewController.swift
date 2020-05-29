7264e3e583c520cd8dc19c56bc3153f1575abd97
Mintimer <mintimer99@gmail.com>
2020-05-29 23:03:48 +0500

    Initial Commit

---
+++ RestaurantRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantRate
+//
+//  Created by Минтимер Харасов on 29.05.2020.
+//  Copyright © 2020 Минтимер Харасов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
