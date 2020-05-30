35c08fbc00881397ba90f4083aea41ad0ca2e632
Mintimer <mintimer99@gmail.com>
2020-05-30 17:53:40 +0500

    Initial

---
+++ RestaurantRate/TableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TableViewController.swift
+//  RestaurantRate
+//
+//  Created by Минтимер Харасов on 29.05.2020.
+//  Copyright © 2020 Минтимер Харасов. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    let restaurants = ["KFC", "Burger King", "McDonalds", "Burger Heroes", "FARШ", "Краснодарский парень"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurants.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = restaurants[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurants[indexPath.row])
+        cell.imageView?.bounds.size = UIImage(named: "KFC")!.size
+
+        return cell
+    }
+
+   
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
